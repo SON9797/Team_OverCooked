10903f50b24b81ea5fc6b343744fdc5b8e4ff7b3
SON9797 <zxcvcxz95@naver.com>
2026-03-20 15:39:05 +0900

    Reapply "Feat : UI시스템 추가"
    This reverts commit 6944521e5ece04ea4e96ae43ec3c57b97ece570a.

---
+++ Assets/Scripts/SHJ/SceneFlowManager.cs
@@ -0,0 +1,69 @@
+using Overcooked.Interfaces;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace Overcooked
+{
+    public class SceneFlowManager : IStartable
+    {
+        private readonly IUIManager _uiManager;
+
+        [Inject]
+        public SceneFlowManager(IUIManager uiManager)
+        {
+            _uiManager = uiManager;
+        }
+
+        public void Start()
+        {
+            _uiManager.SetPanelActive(_uiManager.TutorialPanel, false);
+            _uiManager.SetPanelActive(_uiManager.ReadyPanel, false);
+            _uiManager.SetPanelActive(_uiManager.StartPanel, false);
+            _uiManager.SetPanelActive(_uiManager.CoinPanel, false);
+            _uiManager.SetPanelActive(_uiManager.RecipePanel, false);
+            _uiManager.SetPanelActive(_uiManager.TimerPanel, false);
+
+            _uiManager.StartManagerCoroutine(InitializeFlowCoroutine());
+        }
+
+
+        private IEnumerator InitializeFlowCoroutine()
+        {
+            // 1. 로딩화면
+            _uiManager.SetPanelActive(_uiManager.LoadingPanel, true);
+            yield return new WaitForSeconds(2.0f);
+
+            // 2. 레시피튜토리얼
+            _uiManager.SetPanelActive(_uiManager.LoadingPanel, false);
+            _uiManager.SetPanelActive(_uiManager.TutorialPanel, true);
+
+            // 3. 레시피 튜토리얼에서 Space 입력시 사라지고
+            while (!Input.GetKeyDown(KeyCode.Space))
+            {
+                yield return null; // 스페이스바가 눌릴 때까지 무한 대기
+            }
+            _uiManager.SetPanelActive(_uiManager.TutorialPanel, false);
+
+            // 4. Ready 1.5초
+            _uiManager.SetPanelActive(_uiManager.ReadyPanel, true);
+            yield return new WaitForSeconds(1.5f);
+
+            // 5. Start 1.5초 보여주고
+            _uiManager.SetPanelActive(_uiManager.ReadyPanel, false);
+            _uiManager.SetPanelActive(_uiManager.StartPanel, true);
+            yield return new WaitForSeconds(1.5f);
+
+            // 6. 게임 시작
+            _uiManager.SetPanelActive(_uiManager.StartPanel, false);
+
+            _uiManager.SetPanelActive(_uiManager.CoinPanel, true);
+            _uiManager.SetPanelActive(_uiManager.RecipePanel, true);
+            _uiManager.SetPanelActive(_uiManager.TimerPanel, true);
+
+        }
+    }
+}
