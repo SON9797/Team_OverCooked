e5178361889f87467271f20572ce13afddb5fc00
kangmingoo000805 <kangmingoo000805@gmail.com>
2026-03-23 18:27:46 +0900

    Fix : player 상호작용 타 코드와 맞춤.

--- Assets/Scripts/KMG/PlayerInput/IInGamePlayerInput.cs
+++ Assets/Scripts/KMG/PlayerInput/IInGamePlayerInput.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 
 namespace Overcooked
-{ 
+{
     /// <summary>
     /// 인게임 플레이어 인풋 키 인터페이스. 추가 필요 시 추가 후
     /// InGamePlayerInput(상속받음)에 구현해야함. 
@@ -16,9 +16,9 @@ namespace Overcooked
         Vector2 Move { get; }
         bool DashInput { get; }
         bool SwitchingInput { get; }
-        bool InteractionInput {  get; }
 
-        
+        bool InteractionIngredientInput { get; }
+        bool InteractionCookInput { get; }
 
 
         /*
--- Assets/Scripts/KMG/PlayerInput/InGamePlayerInput.cs
+++ Assets/Scripts/KMG/PlayerInput/InGamePlayerInput.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 
 namespace Overcooked
-{ 
+{
     /// <summary>
     /// 인게임 플레이어 인풋 적용
     /// IInGamePlayerInput 상속받음.
@@ -17,17 +17,23 @@ namespace Overcooked
             {
                 float x = 0f;
                 float y = 0f;
-    
+
                 if (Input.GetKey(KeyCode.W)) y += 1f;
                 if (Input.GetKey(KeyCode.S)) y -= 1f;
                 if (Input.GetKey(KeyCode.D)) x += 1f;
                 if (Input.GetKey(KeyCode.A)) x -= 1f;
-    
+
                 return new Vector2(x, y).normalized;
             }
         }
+
         public bool DashInput => Input.GetKeyDown(KeyCode.LeftAlt);
         public bool SwitchingInput => Input.GetKeyDown(KeyCode.LeftShift);
-        public bool InteractionInput => Input.GetKeyDown(KeyCode.LeftControl);
+
+        // 스페이스 : 재료 상호작용
+        public bool InteractionIngredientInput => Input.GetKeyDown(KeyCode.Space);
+
+        // 레프트컨트롤 : 조리 상호작용
+        public bool InteractionCookInput => Input.GetKeyDown(KeyCode.LeftControl);
     }
 }
