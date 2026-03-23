19c6517f49f272bf8d3756c202105ead50504705
kangmingoo000805 <kangmingoo000805@gmail.com>
2026-03-23 10:28:27 +0900

    ADD : KMG폴더 내 PlayerInput(5)추가, 1-1 스테이지 내 플레이어 추가

---
+++ Assets/Scripts/KMG/PlayerInput/IInGamePlayerInput.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+namespace Overcooked
+{ 
+    /// <summary>
+    /// 인게임 플레이어 인풋 키 인터페이스. 추가 필요 시 추가 후
+    /// InGamePlayerInput(상속받음)에 구현해야함. 
+    /// </summary>
+    public interface IInGamePlayerInput
+    {
+        Vector2 Move { get; }
+        bool DashInput { get; }
+        bool SwitchingInput { get; }
+        bool InteractionInput {  get; }
+    
+    }
+}
