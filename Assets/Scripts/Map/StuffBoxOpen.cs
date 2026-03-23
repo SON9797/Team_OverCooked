d60d211b6ffec64fe78403261d7b2f0ec45aeb28
Shimsebin <sebin502@naver.com>
2026-03-23 15:53:23 +0900

    feat : chop
    다지기 상호작용
    조리대 위에 물건 놓기

--- Assets/Scripts/Map/StuffBoxOpen.cs
+++ Assets/Scripts/Map/StuffBoxOpen.cs
@@ -5,34 +5,39 @@ using UnityEngine;
 public class StuffBoxOpen : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
-    private bool _isPlayerNearby = false;
+    [SerializeField] Transform _rayPoint;
+    [SerializeField] private float _interactionDistance = 3.0f; // 상호작용 가능 거리
 
     void Start()
     {
-        _animator = GetComponent<Animator>();
-       
+        // 만약 에디터에서 할당하지 않았다면 자동으로 가져옴
+        if (_animator == null)
+            _animator = GetComponent<Animator>();
     }
+
     void Update()
     {
-        if (_isPlayerNearby && Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            _animator.SetBool("Open", true);
+            CheckForBox();
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void CheckForBox()
     {
-        if (other.CompareTag("Player"))
-        {
-            _isPlayerNearby = true;
-        }
-    }
+        // 메인 카메라의 위치와 정면 방향을 기준으로 레이 생성
+        RaycastHit hit;
+        
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
+        // 레이를 쏴서 물체에 맞았는지 확인
+        if (Physics.Raycast(_rayPoint.position, _rayPoint.forward, out hit, _interactionDistance))
         {
-            _isPlayerNearby = false;
+            // 맞은 물체가 현재 이 스크립트가 붙은 오브젝트인지 확인
+            if (hit.transform == this.transform)
+            {
+                _animator.SetBool("Open", true);
+                Debug.Log("상자를 열었습니다!");
+            }
         }
     }
 }
