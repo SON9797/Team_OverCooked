7546e55ccd6267f7c0fb63a9c374ac245c4756db
FteDr <cyg68700@naver.com>
2026-03-30 17:06:48 +0900

    Feat: Loby UI

--- Assets/Scripts/UI/MouceHoverChangeImg.cs
+++ Assets/Scripts/UI/MouceHoverChangeImg.cs
@@ -1,16 +1,35 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class MouceHoverChangeImg : MonoBehaviour
+public class MouceHoverChangeImg : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    void Start()
+    [SerializeField] Sprite normalImg;
+    [SerializeField] Sprite hoverImg;
+    [SerializeField] GameObject menu;
+
+    Image img;
+    private void Start()
     {
+        img = GetComponent<Image>();
+        img.sprite = normalImg;
+        menu.SetActive(false);
+    }
+    
         
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        img.sprite = hoverImg;
+        menu.SetActive(true);
     }
 
-    void Update()
+    public void OnPointerExit(PointerEventData eventData)
     {
-        
+        img.sprite = normalImg;
+        menu.SetActive(false);
     }
 }
