2b8bdfb0500b1e4aae60e7bfc69a1db39d9bc5b0
Shimsebin <sebin502@naver.com>
2026-04-06 18:33:23 +0900

    Feat : 월드맵 -스테이지 씬이동

--- Assets/Scripts/Map/Test/BusMove.cs
+++ Assets/Scripts/Map/Test/BusMove.cs
@@ -7,7 +7,14 @@ public class BusMove : MonoBehaviour
     [SerializeField] float _moveSpeed = 5f;
     [SerializeField] private float _turnSpeed = 5f;
 
+    [SerializeField] private float _dashMultiplier = 0.5f;
+    [SerializeField] private float _dashDuration = 1.0f;  
+    [SerializeField] private float _dashCooldown = 1.0f;  
+
     private bool _canMove = false;
+    private bool _isDashing = false;
+    private bool _canDash = true; 
+    private float _currentSpeed;
 
     public static BusMove _instance;
 
@@ -27,6 +34,22 @@ public class BusMove : MonoBehaviour
         }
 
         _rb = GetComponent<Rigidbody>();
+
+        _currentSpeed = _moveSpeed;
+    }
+
+    private void Update()
+    {
+        if (!_canMove)
+        {
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftShift) && _canDash && !_isDashing)
+        {
+            Debug.Log("대쉬 시작!");
+            StartCoroutine(DashRoutine());
+        }
     }
 
     private void FixedUpdate()
@@ -35,10 +58,10 @@ public class BusMove : MonoBehaviour
         {
             return;
         }
-            MoveAndRotate();
+        MoveAndRotate();
     }
 
-    void MoveAndRotate()
+    private void MoveAndRotate()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
@@ -55,13 +78,27 @@ public class BusMove : MonoBehaviour
 
         if (moveDir.magnitude >= 0.1f)
         {
-            _rb.MovePosition(_rb.position + moveDir * _moveSpeed * Time.fixedDeltaTime);
+            _rb.MovePosition(_rb.position + moveDir * _currentSpeed * Time.fixedDeltaTime);
 
             Quaternion targetRotation = Quaternion.LookRotation(moveDir);
             targetRotation *= Quaternion.Euler(0, -90, 0);
             _rb.rotation = Quaternion.Slerp(_rb.rotation, targetRotation, _turnSpeed * Time.fixedDeltaTime);
         }
     }
+    private IEnumerator DashRoutine()
+    {
+        _isDashing = true;
+        _canDash = false;
 
+        _currentSpeed = _moveSpeed * _dashMultiplier;
+
+        yield return new WaitForSeconds(_dashDuration);
+
+        _currentSpeed = _moveSpeed;
+        _isDashing = false;
+
+        yield return new WaitForSeconds(_dashCooldown);
+        _canDash = true;
+    }
 
 }
