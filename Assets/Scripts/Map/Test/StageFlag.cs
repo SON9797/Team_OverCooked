d2dd6a48cc8410251fc3179f39d3ebae42a0859a
Shimsebin <sebin502@naver.com>
2026-04-09 17:55:45 +0900

    Feat : 디버그로그 지움

--- Assets/Scripts/Map/Test/StageFlag.cs
+++ Assets/Scripts/Map/Test/StageFlag.cs
@@ -11,20 +11,14 @@ public class StageFlag : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{other.name}이(가) 들어옴");
 
         BusMove bus = other.GetComponentInParent<BusMove>();
         if (bus != null)
         {
             if (_transUI != null)
             {
-                Debug.Log("BusMove 컴포넌트 찾음!");
                 _transUI.ShowUI(_stageKey);
             }
-        }
-        else
-        {
-            Debug.Log("BusMove를 찾지 못했습니다.");
         }
     }
 
